60b1519fdfc04dfd5c1c9302bf743b83f94eea86
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-03-24 10:01:18 +0100

    Add files via upload

---
+++ examen prograaaaa.py
@@ -0,0 +1,12 @@
+class Libro:
+    def __init__(self,titulo,autor,paginas,año_publicacion):
+        self.titulo= titulo
+        self.autor = autor
+        self.paginas = paginas
+        self.año_publicacion = año_publicacion
+
+
+#leer valores por teclado
+
+libro1 = Libro("Aventurasde juan", "Mario Ferc¡nandez", 194, 1967)
+
